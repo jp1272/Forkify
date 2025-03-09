fce403184a1d15bf217364b6dbea37278845dfff
jp1272 <jp.deaton@vikings.berry.edu>
2025-03-09 17:00:12 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '559986a1-73c6-4e1e-b52d-72057c94cf8d';
+export const MODAL_CLOSE_SEC = 2.5;
