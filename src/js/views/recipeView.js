a8485ff64ea2e9a65d27def41bdd524bc50b43e2
jp1272 <jp.deaton22@gmail.com>
2025-03-10 10:00:31 -0400

    trying to fix deploy bugs

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
 // import icons from '../img/icons.svg' // parcel 1
-import icons from './icons.svg'; // parcel 2
+import icons from 'url:../../img/icons.svg'; // parcel 2
 import fracty from 'fracty';
 
 class RecipeView extends View {
@@ -129,7 +129,7 @@ class RecipeView extends View {
     <use href="${icons}#icon-check"></use>
   </svg>
   <div class="recipe__quantity">${
-    ing.quantity ? new fracty(ing.quantity).toString() : ''
+    ing.quantity ? fracty(ing.quantity).toString() : ''
   }</div>
   <div class="recipe__description">
     <span class="recipe__unit">${ing.unit}</span>
